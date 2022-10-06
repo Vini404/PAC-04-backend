a59d21c815ed9265c2d773ad9950a78d10f0f8dc
vinicius.ochner <vinicius.ochner@fulltechpro.com.br>
2022-10-06 19:29:18 -0300

    feat: corrigindo dockerfile e implementando health check

---
+++ src/WebApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new OkResult();
+        }
+    }
+}
